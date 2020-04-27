496714b1646fbde646c8ae8d445b0fc437072a52
Alan-Kuan <kuanhaoan@gmail.com>
2020-04-27 12:10:50 +0800

    Complete plus and minus function.

--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -16,9 +16,9 @@ BigInteger BigInteger::plus(BigInteger b)
 
 	size_t max_len = MAX(len1, len2);
 
-	res.num_literal.resize(max_len + 1);
+	res.num_literal.resize(max_len);
 
-	int carry = 0;
+	Digit carry = 0;
 
 	for(size_t i = 0; i < max_len; i++){
 
@@ -41,15 +41,78 @@ BigInteger BigInteger::plus(BigInteger b)
 	}
 
 	if(carry == 1)
-		res.num_literal[max_len] = 1;
-	else
-		res.num_literal.pop_back();
+		res.num_literal.push_back(1);
 
 	return res;
 }
 
-BigInteger minus(BigInteger b)
+BigInteger BigInteger::minus(BigInteger b)
 {
+    BigInteger res;
 
+    size_t len1 = num_literal.size();
+    size_t len2 = b.num_literal.size();
 
+    size_t max_len = MAX(len1, len2);
+
+    res.num_literal.resize(max_len);
+
+    if(*this >= b){
+
+        Digit negative_carry = 0;
+
+        for(size_t i = 0; i < len1; i++){
+
+            if(i >= len2)
+                res.num_literal[i] = num_literal[i] + negative_carry;
+            else
+                res.num_literal[i] = num_literal[i] - b.num_literal[i] + negative_carry;
+
+            if(res.num_literal[i] < 0){
+
+                res.num_literal[i] += 10;
+
+                negative_carry = -1;
+
+            }else
+                negative_carry = 0;
+
+        }
+
+    }else{
+
+        res.is_negative = true;
+
+        Digit negative_carry = 0;
+
+        for(size_t i = 0; i < len2; i++){
+
+            if(i >= len1)
+                res.num_literal[i] = b.num_literal[i] + negative_carry;
+            else
+                res.num_literal[i] = b.num_literal[i] - num_literal[i] + negative_carry;
+
+            if(res.num_literal[i] < 0){
+
+                res.num_literal[i] += 10;
+
+                negative_carry = -1;
+
+            }else
+                negative_carry = 0;
+
+        }
+
+    }
+
+    for(size_t i = max_len - 1; i >= 0; i--){
+
+        if(res.num_literal[i] == 0)
+            res.num_literal.pop_back();
+        else
+            break;
+
+    }
+
+    return res;
 }
